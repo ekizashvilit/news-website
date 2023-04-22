15ea2ce8606b6c1f52bec1ea9d7de874992b1356
Teona <tekizashvili@gmail.com>
2023-04-22 20:52:52 +0400

    added prev and next buttons, functionality is done

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -1,4 +1,21 @@
+import { useGlobalContext } from './context';
+
 const Buttons = () => {
-  return <div>Buttons</div>;
+  const { handlePage, isLoading, page, nbPages } = useGlobalContext();
+
+  return (
+    <div className="btn-container">
+      <button disabled={isLoading} onClick={() => handlePage('dec')}>
+        prev
+      </button>
+      <p>
+        {page + 1} of {nbPages}
+      </p>
+      <button disabled={isLoading} onClick={() => handlePage('inc')}>
+        next
+      </button>
+    </div>
+  );
 };
+
 export default Buttons;
