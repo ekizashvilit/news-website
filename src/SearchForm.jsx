f193c80a1cca0b6ac3e4d0df26eb4fef0812740c
Teona <tekizashvili@gmail.com>
2023-04-22 20:23:14 +0400

    set up search form

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,4 +1,18 @@
+import { useGlobalContext } from './context';
+
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const { query, handleSearch } = useGlobalContext();
+
+  return (
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+      <h2>search news</h2>
+      <input
+        type="text"
+        className="form-input"
+        value={query}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
+    </form>
+  );
 };
 export default SearchForm;
